f31b4265777effda095a3149add7168e81cc9d5e
somiyaratna <beherasomiyaratna@gmail.com>
2024-09-16 20:49:51 +0530

    Basic Navigation

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,14 +1,35 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import PlaceItem from "./PlaceItem";
 
 const PlacesList = ({ places }) => {
+  if (!places || places.length === 0) {
+    return (
+      <View style={styles.fallbackContainer}>
+        <Text style={styles.fallbackText}>
+          No places added yet, Start adding some!
+        </Text>
+      </View>
+    );
+  }
   return (
-    <FlatList data={places} keyExtractor={(item) => item.id}>
-      <Text>PlacesList</Text>
-    </FlatList>
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <PlaceItem place={item} />}
+    ></FlatList>
   );
 };
 
 export default PlacesList;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  fallbackContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fallbackText: {
+    fontSize: 16,
+  },
+});
