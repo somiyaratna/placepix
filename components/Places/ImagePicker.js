ddf863e791368139f50dc9bf15b6230183753572
somiyaratna <beherasomiyaratna@gmail.com>
2024-09-17 21:40:35 +0530

    ImagePicker added

---
+++ components/Places/ImagePicker.js
@@ -0,0 +1,73 @@
+import { Alert, Button, Image, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+import { Colors } from "../../constants/colors";
+
+const ImagePicker = () => {
+  const [pickedImage, setPickedImage] = useState();
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert("Need camera access to take images");
+      return false;
+    }
+
+    return true;
+  }
+  async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    setPickedImage(image.assets[0].uri);
+  }
+
+  return (
+    <View>
+      <View style={styles.imagePreview}>
+        {pickedImage ? (
+          <Image style={styles.image} source={{ uri: pickedImage }} />
+        ) : (
+          <Text>Its your favorite place. Take a pic!!</Text>
+        )}
+      </View>
+      <Button title="Take Image" onPress={takeImageHandler} />
+    </View>
+  );
+};
+
+export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
